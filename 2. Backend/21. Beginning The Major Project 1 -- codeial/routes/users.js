f35d1a86627f6d1c5852d3682cac5fb7935c00aa
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2022-06-29 13:34:56 +0530

    Delete 2. Backend/21. Beginning The Major Project 1 -- codeial directory

--- 2. Backend/21. Beginning The Major Project 1 -- codeial/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router=express.Router();
-
-const usersController=require('../controllers/users_controller');
-
-router.get('/usersProfile' ,usersController.usersProfile);
-
-module.exports=router;
