4fe457f0816db411b7982f942c92ffb534509b1c
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2022-06-29 13:31:29 +0530

    Add files via upload

---
+++ 2. Backend/10. Beginning The Major Project 1 -- codeial/controllers/users_controller.js
@@ -0,0 +1,8 @@
+module.exports.profile = function(req,res){
+    return res.end('<h1> Users profile<h1>');
+}
+
+//usersProfile
+module.exports.usersProfile=function(req,res){
+    return res.end( "<h1> Hello this is the users profile<h1> ");
+}
