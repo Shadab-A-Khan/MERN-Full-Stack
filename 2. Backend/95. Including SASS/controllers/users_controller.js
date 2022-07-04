f77913b0d4146f4cf8c9a2a89aafa9d3987ca8e8
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2022-07-04 14:56:40 +0530

    Add files via upload

---
+++ 2. Backend/95. Including SASS/controllers/users_controller.js
@@ -0,0 +1,83 @@
+const User = require("../models/user");
+
+module.exports.profile = function(req,res){
+    return res.end('<h1> Users profile <h1>');
+}
+
+//usersProfile
+module.exports.usersProfile=function(req,res){
+
+    return res.render("user_profile",{
+        title:"profile"
+    });
+}
+
+
+//render sign-in page
+module.exports.userSignIn=function(req,res){
+
+    if(req.isAuthenticated()){
+        res.redirect('/users/usersProfile');
+    }
+    
+    return res.render('user_sign_in',{
+        title:"codeial/sign in"
+    });
+}
+
+//render sign-up page
+module.exports.userSignUp=function(req,res){
+
+    if(req.isAuthenticated()){
+        res.redirect('/users/usersProfile');
+    }
+
+    return res.render('user_sign_up',{
+        title:"codeial/sign up"
+    });
+}
+
+//get the sign-up data
+module.exports.create=function(req, res){
+    console.log(req.body);
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err,user){ 
+        if(err){console.log('error in finding user in signing up'); return}
+    
+        if(!user){
+            User.create(req.body, function(err,user){
+                if(err){console.log('error in creating user while signing up',err); return}
+
+                return res.redirect('/users/sign_in');
+            })
+        } else{
+                return res.redirect('back');    
+              }
+    });
+}
+
+// module.exports.createSession = function( req, res){
+//     req.logout();
+
+//     return res.redirect('/');
+// }
+
+//render createSession
+ module.exports.createSession=function(req,res){
+    //req.logout();
+    return res.redirect('/');
+ }
+
+ module.exports.destroySession=function(req,res){
+
+    req.logout(function(err) {
+        if (err) {
+             return next(err); 
+            }
+        res.redirect('/');
+      });
+ }
+
---
+++ 2. Backend/95. Including SASS/index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const expressLayout=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+const bodyParser = require('body-parser'); 
+//passport-authentication
+//used for session cookie
+const session = require('express-session'); //encprypts the id
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware=require('node-sass-middleware');
+const { where } = require('./models/user');
+
+const app = express();
+
+// sass/scss middleware for making css (styling easy)
+app.use(sassMiddleware({
+  src: './assets/scss',   //from where the scss will be picked to be converted to css
+  dest: './assets/css'  , //from where the css to be picked
+  debug: 'true',  //to show the err while unable to convert to css
+  outputStyle: 'extended', //we want it to be in multiple line and understandable
+  prefix: '/css'
+}));
+
+
+//middle ware to the form post data 
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('./assets'));
+
+app.use(expressLayout);
+
+app.use(bodyParser.json());
+
+
+
+
+//middleware to use the cookieParser
+app.use(cookieParser());
+
+
+//extract styles and script from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//passport
+//session ,here we are encrypting the id
+
+// app.use(session({
+//     name: 'codeial',
+//     // TODO change the secret before deployment in production mode
+//     secret: 'blahsomething',
+//     saveUninitialized: false,
+//     resave: false,
+//     cookie: {
+//         maxAge: (1000 * 60 * 100)
+//     },
+//     store: new MongoStore(
+//         {
+//             mongooseConnection: db,
+//             autoRemove: 'disabled'
+        
+//         },
+//         function(err){
+//             console.log(err ||  'connect-mongodb setup ok');
+//         }
+//     )
+// }));
+
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store:MongoStore.create({
+        mongoUrl:db._connectionString,
+        autoRemove: 'disabled'
+      })
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser)
+//use express router
+app.use('/', require('./routes'));
+
+const port=8000;
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in running the server: ${err}`);
+    }
+    console.log(`server is runnig on port: ${port}`);
+});
