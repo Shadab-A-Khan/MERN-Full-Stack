7c94fe924db4d050bab691f56f20f17e3ddcff24
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2022-07-12 14:23:14 +0530

    Add files via upload

---
+++ 2. Backend/98. APIs (making APIs and JWT authentication)/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const usersController = require('../controllers/users_controller');
+
+router.get('/usersProfile/:id', passport.checkAuthentication, usersController.usersProfile);
+//check authentication will ensure that user is signed in ,
+//then only he can get to the profile page
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
+
+router.get('/sign_in', usersController.userSignIn);
+router.get('/sign_up', usersController.userSignUp);
+
+//creating a user's account
+router.post('/create', usersController.create);
+
+//use passport as a middleware to authenticatee
+router.post('/create-session', passport.authenticate(
+    'local',
+    { failureRedirect: '/users/sign_in'},
+), usersController.createSession);
+
+router.get('/sign_out', usersController.destroySession);
+
+module.exports = router;
